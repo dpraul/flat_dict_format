6f619343ac3d3b82041a0755fa3d6a1a812b602c
Dylan Praul <dylan.praul@gmail.com>
2017-03-30 18:43:07 -0400

    Remove Py3 type-hinting for back-support for Py2, kick off travis.

--- fdf/load.py
+++ fdf/load.py
@@ -6,17 +6,39 @@ DEFAULT_DICT_DELIMITER = ':'
 DEFAULT_KEY_DELIMITER = '.'
 
 
-def load(fp: TextIOWrapper, key_delimiter: str = DEFAULT_KEY_DELIMITER,
-         dict_delimiter: str = DEFAULT_DICT_DELIMITER) -> dict:
+def load(fp, key_delimiter=DEFAULT_KEY_DELIMITER, dict_delimiter=DEFAULT_DICT_DELIMITER):
+    """ Load dictionary from FDF file in file-like object
+
+    :param TextIOWrapper fp: the file-like object to open
+    :param str key_delimiter: the delimiter between the key and value
+    :param str dict_delimiter: the delimiter between nested dictionary entries
+    :return:
+    :rtype: dict
+    """
     return loads(fp.read(), key_delimiter, dict_delimiter)
 
 
-def loads(s: str, key_delimiter: str = DEFAULT_KEY_DELIMITER,
-          dict_delimiter: str = DEFAULT_DICT_DELIMITER) -> dict:
+def loads(s, key_delimiter=DEFAULT_KEY_DELIMITER, dict_delimiter=DEFAULT_DICT_DELIMITER):
+    """ Load dictionary from FDF file stored in string variable
+
+    :param str s: the string to load a dictionary from
+    :param str key_delimiter: the delimiter between the key and value
+    :param str dict_delimiter: the delimiter between nested dictionary entries
+    :return:
+    :rtype: dict
+    """
     return decode(s.splitlines(), key_delimiter, dict_delimiter)
 
 
-def decode(lines: iter, key_delimiter: str, dict_delimiter: str) -> dict:
+def decode(lines, key_delimiter, dict_delimiter):
+    """ Extract dictionary FDF from iterable of lines.
+
+    :param iter lines: lines to decode
+    :param str key_delimiter: the delimiter between the key and value
+    :param str dict_delimiter: the delimiter between nested dictionary entries
+    :return:
+    :rtype: dict
+    """
     result = {}
     for i, line in enumerate(lines):
         if len(line.strip()) == 0:  # ignore empty lines
